e106c4da3f1b85bcd91d2e477a0c052d893090b7
crystal-triode <olega2111@yandex.ru>
2022-08-07 21:39:15 +0300

    add HomeTask_004

---
+++ HomeTask_004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число 1 ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2 ");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 3 ");
+int numberC = int.Parse(Console.ReadLine());
+int max = numberA;
+if(numberB > max) max = numberB;
+if(numberC > max) max = numberC;
+Console.Write("Максимальное число = "); 
+Console.WriteLine(max);
