98364dffbd62cb8dcaaace9a5d127d42526d213b
crystal-triode <olega2111@yandex.ru>
2022-08-08 21:18:04 +0300

    add HomeTask_008

---
+++ HomeTask_008/Program.cs
@@ -0,0 +1,21 @@
+﻿int i = 1;
+bool not = true;
+
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Чётные числа от 1 до " + N);
+while (i <= N && N > 0)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write(i + ", ");
+        not = false;
+    }
+    i++;
+}
+
+if (not)
+{
+    Console.WriteLine("Нет чётных чисел!");
+}
