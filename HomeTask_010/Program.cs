5abe12af7d227819248f1cb1a957d77a92fed727
crystal-triode <olega2111@yandex.ru>
2022-08-11 23:05:42 +0300

    add HomeTask_010

---
+++ HomeTask_010/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+string str = Console.ReadLine();
+if (str.Length <= 3 && str.Length > 2)
+{
+    Console.Write("Вторая цифра числа - ");
+    Console.WriteLine(str[1]);
+}
+else
+{
+    Console.Write("Число НЕ трёхзначное ");
+}
