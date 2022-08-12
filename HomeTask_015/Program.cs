0ccbaebbf99d85ddd41412495149d63d5702eacc
crystal-triode <olega2111@yandex.ru>
2022-08-12 21:52:34 +0300

    add HomeTask_015

---
+++ HomeTask_015/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число, обозначающее день недели (от 1 до 7)");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 6 || num == 7)
+{
+    Console.WriteLine("Ура! Это же выходной");
+}
+else if (num >= 7 || num < 0)
+{
+    Console.WriteLine("Такого дня недели нет");
+}
+else
+{
+    Console.WriteLine("Печально! Нужно идти на работу");
+}
