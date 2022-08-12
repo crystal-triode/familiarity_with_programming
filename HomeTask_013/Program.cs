dc527cf01903de5b0dbcb88944bd9f7c63a777cf
crystal-triode <olega2111@yandex.ru>
2022-08-12 21:26:05 +0300

    add Hometask_013

---
+++ HomeTask_013/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+string str = Console.ReadLine();
+if (str.Length >= 3)
+{
+    Console.WriteLine("Третья цифра числа - ");
+    Console.WriteLine(str[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры НЕТ");
+}
