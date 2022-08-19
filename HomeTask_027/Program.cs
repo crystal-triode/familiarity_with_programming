36b7f8cf8fe789c4162e4174d2cf7900018315cc
crystal-triode <olega2111@yandex.ru>
2022-08-19 20:03:40 +0300

    add HomeTask_027

---
+++ HomeTask_027/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Write("Введите число : ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Sum (int N)
+{
+    int Length = Convert.ToString(N).Length;
+    int dif = 0;
+    int result = 0;
+
+    for (int i = 0; i < Length; i++)
+    {
+        dif = N - N % 10;
+        result = result + (N - dif);
+        N = N / 10;
+    }
+    return result;
+}
+
+int sum = Sum(N);
+Console.WriteLine("Сумма цифр в числе: " + sum);
