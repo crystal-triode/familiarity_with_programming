8c18debcde2db46ce71a376d8e0d38ff6605a311
crystal-triode <olega2111@yandex.ru>
2022-08-23 22:44:18 +0300

    add HomeTask_036

---
+++ HomeTask_036/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int [] generateArray ( int lenght = 6, int min = 10, int max = 100)
+{
+    int[] result = new int[lenght];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(min, max);
+    }
+    return result;
+}
+int [] array = generateArray();
+
+int sumOfOdd(int[] array) 
+    {
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++) 
+        {
+        if(i % 2 != 0)
+        {
+            sum += array[i];
+        }
+        }
+        return sum;
+    }
+    int Sum = sumOfOdd (array);
+Console.WriteLine ($"Сгенерированный массив :[{string.Join (",", array)}]");
+Console.WriteLine ("Сумма нечётных позиций массива: " + Sum);
