07f9e62b94cbbe1e2ce35982da8b5c8a30a3c2f7
crystal-triode <olega2111@yandex.ru>
2022-08-25 19:29:21 +0300

    add HomeTask_041

---
+++ HomeTask_041/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите положительные и отрицательные числа через пробел ");
+int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество положительных чисел: {count}");
