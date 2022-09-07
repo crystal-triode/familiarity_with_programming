95e154e723fa0fc8036b84de3e9066e679577993
crystal-triode <olega2111@yandex.ru>
2022-09-07 13:53:54 +0300

    add HomeTask_064

---
+++ HomeTask_064/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+Console.WriteLine("Введите число M:");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine());
+int count = N;
+NaturalNumbers(M, count);
+
+void NaturalNumbers(int M, int count)
+{
+    if(M > count)
+    {
+        return;
+    }
+    else
+    {
+        NaturalNumbers(M, count - 1);
+        Console.Write(count + " ");
+    }
+}
