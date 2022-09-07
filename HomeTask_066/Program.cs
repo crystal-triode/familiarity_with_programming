8fc0b7836e0d76e6eb37d6ec4dbd9a711841fd58
crystal-triode <olega2111@yandex.ru>
2022-09-07 14:30:32 +0300

    add HomeTask_066

---
+++ HomeTask_066/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+SumOfNaturalNumbers(M, N);
+
+void SumOfNaturalNumbers(int M, int N)
+{
+    Console.Write(SumMN(M - 1, N));
+}
+
+int SumMN(int M, int N)
+{
+    int res = M;
+    if (M == N)
+        return 0;
+    else
+    {
+        M++;
+        res = M + SumMN(M, N);
+        return res;
+    }
+}
